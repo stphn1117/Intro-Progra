1e2ca542a83870ddc7eba56954be616a3fda4289
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-08-01 18:55:13 -0500

    prueba de uso de clases

--- prueba de Git/src/prueba.java
+++ prueba de Git/src/prueba.java
@@ -1,6 +1,19 @@
-
+//clase prueba con un contructor que inicializa el nombre
 public class prueba {
- public static void main(String[] args) {
-	 System.out.println("HOLA");
- }
-}
+	private String nombre;//variable de instancia
+	//el contructor inicializa el nombre con el parametro nombre
+	public prueba(String nombre) 
+	{//el nombre del constructor es el nombre de la clase
+		this.nombre = nombre;
+	}
+//metodo para establecer el nombre
+	public void establecerNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+ //metodos para recuperar el nombre
+	public String obtenerNombre()
+	{
+	 return nombre;
+	}
+}// fin de la clase
---
+++ prueba de Git/src/prueba2.java
@@ -0,0 +1,17 @@
+//uso del contructor de prueba.java para inicializar la variable de instacia
+//nombre al momento d crear el objeto prueba
+public class prueba2
+{
+	public static void main(String[] args)
+	{
+		//crear dos objetos
+		prueba cuenta1 = new prueba("Jane Green");
+		prueba cuenta2 = new prueba("John Blue");
+	   //muestra el valor inicial de nombre para cada cuenta
+		System.out.printf("el nombre de la persona es: %s%n", cuenta1.obtenerNombre());
+		System.out.printf("el nombre de la persona es: %s%n", cuenta2.obtenerNombre());
+		
+	}
+
+}
+//fin de la clase prueba2
